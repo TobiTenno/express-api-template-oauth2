542d7313dd1001b25420e7976645daa87c0eda39
Tobiah <tobiah@pm.me>
2021-06-03 23:20:45 -0500

    chore: remove unneeded dependency

--- src/spec/hooks/db.js
+++ src/spec/hooks/db.js
@@ -5,11 +5,8 @@ process.env.SECRET_KEY = 'superduperpassword';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const flatCache = require('flat-cache');
 
 const mockDB = require('../mocks/mockDB');
-
-const cache = flatCache.load('mocha');
 
 let User;
 let Example;
@@ -32,8 +29,6 @@ module.exports.mochaHooks = {
     await Example.deleteMany({});
   },
   async afterAll() {
-    cache.removeKey('user');
-    cache.removeKey('token');
     return mockDB.close();
   },
 };
