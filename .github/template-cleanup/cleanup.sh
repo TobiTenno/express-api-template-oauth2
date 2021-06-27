8ea8bb90aec4828bf701fae4945dbc0212dd808b
Tobiah <tobiah@pm.me>
2021-06-26 23:12:38 -0500

    update sed options

--- .github/template-cleanup/cleanup.sh
+++ .github/template-cleanup/cleanup.sh
@@ -7,13 +7,14 @@ export LANG=C
 NAME="${GITHUB_REPOSITORY##*/}"
 SAFE_NAME=$(echo $NAME | sed 's/[^a-zA-Z0-9]//g' | tr '[:upper:]' '[:lower:]')
 
+
 # Replace placeholders in the template-cleanup files
 sed -i "s/%NAME%/$NAME/g" .github/template-cleanup/*
-sed -i "s/%GITHUB_REPOSITORY%/$GITHUB_REPOSITORY/g" .github/template-cleanup/*
+sed -i "s^%GITHUB_REPOSITORY%^$GITHUB_REPOSITORY^g" .github/template-cleanup/*
 
 # Replace template artifact name in project files with $NAME
 find src -type f -exec sed -i "s/express-api-template/$SAFE_NAME/g" {} +
-find src -type f -exec sed -i "s/express-template/$SAFE_NAME/g" {} +
+find src -type f -exec sed -i "s/express-template/$NAME/g" {} +
 find src -type f -exec sed -i "s/express-api-template-oauth2/$SAFE_NAME/g" {} +
 
 # Move content
