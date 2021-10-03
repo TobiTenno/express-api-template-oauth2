22dd4daef84feb8d7fefd709eeff1edffd62d48a
Matej Voboril <tobiah@pm.me>
2021-10-03 00:56:44 -0500

    feat: remove more examples on generation

--- .github/template-cleanup/src/app/controllers/index.js
+++ .github/template-cleanup/src/app/controllers/index.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const path = require('path');
+const redoc = require('redoc-express');
 const router = require('express').Router();
 
+const definition = require('../../scripts/swaggerDefinition');
+
 router.use('/', require('./root'));
+
+router.get('/docs/openapi.yaml', (req, res) => res.sendFile(path.join(__dirname, '../../../', 'openapi.yaml')));
+router.get('/docs', redoc({
+  title: definition.info.title,
+  specUrl: '/docs/openapi.yaml',
+}));
+
 router.use('/users', require('./users'));
 
 module.exports = router;
