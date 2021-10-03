5a43e518e114f2439a3e550cfe6e3974d2dc3a5b
Matej Voboril <tobiah@pm.me>
2021-10-03 01:11:02 -0500

    cache yaml

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -4,11 +4,17 @@ const path = require('path');
 const redoc = require('redoc-express');
 const router = require('express').Router();
 
+const cache = require('apicache').options({
+  appendKey: (req) => `${req.platform}${req.language}` || '',
+}).middleware;
+
 const definition = require('../../scripts/swaggerDefinition');
 
 router.use('/', require('./root'));
 
-router.get('/docs/openapi.yaml', (req, res) => res.sendFile(path.join(__dirname, '../../../', 'openapi.yaml')));
+router.get('/docs/openapi.yaml',
+  cache('24 hours'),
+  (req, res) => res.sendFile(path.join(__dirname, '../../../', 'openapi.yaml')));
 router.get('/docs', redoc({
   title: definition.info.title,
   specUrl: '/docs/openapi.yaml',
